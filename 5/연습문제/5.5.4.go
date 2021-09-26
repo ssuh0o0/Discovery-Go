6ea16dc6f8ddc2d65c028008062c4c9167a49627
ssuh0o0 <ssuh0o0@daum.net>
2021-09-26 21:09:18 +0900

    go

---
+++ 5/연습문제/5.5.4.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type status int
+
+const (
+	UNKNOWN status = iota
+	TODO
+	DONE
+)
+
+type Deadline struct {
+	time.Time
+}
+
+type Task struct {
+	Title    string    `json:"title,omitempty"`
+	Status   status    `json:"status,omitempty"`
+	Deadline *Deadline `json:"deadline,omitempty"`
+	Priority int       `json:"priority,omitempty"`
+	SubTasks []Task    `json:"subTasks,omitempty"`
+}
+
+type Interface interface {
+	Len() int
+	Less(i, j int) bool
+	Swap(i, j int)
+}
+
+type sortPriority int
+
+func (c sortPriority) Len() int {
+	return len(c)
+}
+
+func (c sortPriority) Less(i, j int) bool {
+	return i < j || i == j && i < j
+}
+
+func (c sortPriority) Swap(i, j int) {
+	i, j = c[j], c[i]
+}
+
+func (t Task) String() string {
+	check := "v"
+	if t.Status != DONE {
+		check = " "
+	}
+	return fmt.Sprintf("[%s] %s %s", check, t.Title, t.Deadline)
+}
+
+type IncludeSubTasks Task
+
+func (t IncludeSubTasks) indentedString(prefix string) string {
+	str := prefix + Task(t).String()
+	for _, st := range t.SubTasks {
+		str += "\n" + IncludeSubTasks(st).indentedString(prefix+"  ")
+	}
+	return str
+}
+
+func (t IncludeSubTasks) String() string {
+	return t.indentedString("")
+}
